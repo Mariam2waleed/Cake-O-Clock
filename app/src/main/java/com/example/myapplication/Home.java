1d833f682d4abe65ac9f853fa0cea9e5072dee67
Mariam waleed <mariam2waleed@gmail.com>
2022-04-28 01:54:06 +0200

    Calling

--- app/src/main/java/com/example/myapplication/Home.java
+++ app/src/main/java/com/example/myapplication/Home.java
@@ -1,21 +1,41 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Home extends AppCompatActivity {
 
     private Button menu;
+    private static final int Request_Call = 1;
+    private TextView phone;
+    private ImageView call;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
 
+        phone = findViewById(R.id.phoneNo);
+        call = findViewById(R.id.phone);
+        call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calling();
+            }
+        });
 
         menu = (Button) findViewById(R.id.menu);
         menu.setOnClickListener(new View.OnClickListener() {
@@ -27,7 +47,31 @@ public class Home extends AppCompatActivity {
         });
 
     }
-    public void openCategories(){
+
+    private void Calling() {
+        String Number = phone.getText().toString();
+        if (Number.trim().length() > 0) {
+            if (ContextCompat.checkSelfPermission(Home.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(Home.this, new String[]{Manifest.permission.CALL_PHONE}, Request_Call);
+            } else {
+                String dial = "tel:" + Number;
+                startActivity(new Intent(Intent.ACTION_CALL, Uri.parse(dial)));
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == Request_Call) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Calling();
+            } else {
+                Toast.makeText(this, "permission DENIED", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    public void openCategories() {
         Intent c = new Intent(this, categories.class);
         startActivity(c);
     }
