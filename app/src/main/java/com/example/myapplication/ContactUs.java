bcc9a61cc51ec07dc404f46ab024cca1ebcfa8b7
NouraAbououf279 <nourakhaledouf@gmail.com>
2022-04-27 13:35:07 +0200

    Test nourhan

--- app/src/main/java/com/example/myapplication/ContactUs.java
+++ app/src/main/java/com/example/myapplication/ContactUs.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class ContactUs extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class ContactUs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.Contact_Us);
+
+        Log.v("","");
     }
 }
