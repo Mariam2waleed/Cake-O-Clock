41efe2d91424c6e574b41f26296bca80c7d2fe09
Mariam waleed <mariam2waleed@gmail.com>
2022-04-27 12:50:27 +0200

    Test commit

--- app/src/main/java/com/example/myapplication/MainActivity7.java
+++ app/src/main/java/com/example/myapplication/MainActivity7.java
@@ -9,6 +9,6 @@ public class MainActivity7 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main7);
+        setContentView(R.layout.activity_main2);
     }
 }
