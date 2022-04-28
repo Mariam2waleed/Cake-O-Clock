dbb96650215e1efd3f256167edd33a47ae658994
Mariam waleed <mariam2waleed@gmail.com>
2022-04-28 23:26:09 +0200

    home

--- app/src/main/java/com/example/myapplication/cakes.java
+++ app/src/main/java/com/example/myapplication/cakes.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
